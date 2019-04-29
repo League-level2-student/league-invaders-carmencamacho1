df48f7634094088cb6aac43286710a7cd1a77fbc
Carmen Camacho <Carmen.Camacho@gompersstudent.org>
2019-04-29 14:32:17 -0700

    LeagueInvaders, GamePanel

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,25 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class LeagueInvaders {
-
+	GamePanel gPanel;
+	JFrame frame;
+	public static final int width = 500;
+	public static final int height = 800; 
+public static void main(String[] args) {
+	LeagueInvaders game = new LeagueInvaders();
+	game.setup();
+}
+LeagueInvaders(){
+	gPanel = new GamePanel();
+	frame = new JFrame();
+	
+}
+void setup(){
+	frame.add(gPanel);
+	frame.setSize(width, height);
+	frame.setVisible(true);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+}
 }
