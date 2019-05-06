f2fe5bf0a3eed60da1f76585c8f6a13b0d7b7a60
Jason Tablas <Jason.Tablas@gompersstudent.org>
2019-05-06 14:29:49 -0700

    LeagueInvaders

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -3,11 +3,13 @@ import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class GamePanel extends JPanel implements ActionListener {
+public class GamePanel extends JPanel implements ActionListener, KeyListener {
     final int MENU = 0;
     final int GAME = 1;
     final int END = 2; 
@@ -74,6 +76,21 @@ public class GamePanel extends JPanel implements ActionListener {
 		}
 		System.out.println("action");
 		repaint();
+		
+	}
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
 		
 	}
 }
